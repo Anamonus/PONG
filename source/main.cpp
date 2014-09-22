4afc9abcc05b5d326c3bf3e2b99ca6ccc3fd7598
Anamonus <das316@hotmail.com>
2014-09-22 15:52:06 -0700

    First Commit

---
+++ source/main.cpp
@@ -0,0 +1,332 @@
+﻿#include "AIE.h"
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <stdio.h>   
+#include <stdlib.h> 
+#include <Windows.h>
+#include <Windowsx.h>
+
+using namespace std;
+enum GAMESTATES
+{
+	MENU,
+	GAMEPLAY,
+	INSTRUCTIONS,
+};
+
+int screenWidth = 800;
+int screenHeight = 600;
+int speed = 500;
+int scoreBlue = 0;
+int scoreRed = 0;
+//Player scores and display
+char bufferRed[10] = "0";
+char bufferBlue[10] = "0";
+float ballSpeedX = 0.35;
+float ballSpeedY = 0.35;
+bool gameRunning = true;
+int highScore;
+double iMouseY;
+double iMouseX;
+
+
+struct Button
+{
+	float bX, bY, width, height;
+	unsigned int spriteId;
+	bool clicked = false;
+};
+
+struct Player
+{
+	unsigned int spriteId;
+	float width;
+	float height;
+	void setSize(float a_width, float a_height)
+	{
+		width = a_width;
+		height = a_height;
+	}
+	float x;
+	float y;
+	void setPosition(float a_x, float a_y)
+	{
+		x = a_x;
+		y = a_y;
+		MoveSprite(spriteId, x, y);
+	}
+	unsigned int moveUpKey;
+	unsigned int moveDownKey;
+	void setMovementKeys(unsigned int a_moveUpKey, unsigned int a_moveDownKey)
+	{
+		moveDownKey = a_moveDownKey;
+		moveUpKey = a_moveUpKey;
+	}
+	unsigned int upExtreme;
+	unsigned int downExtreme;
+	void setExtremes(float a_upExtreme, float a_downExtreme)
+	{
+		upExtreme = a_upExtreme;
+		downExtreme = a_downExtreme;
+	}
+	void Move(float a_fTimeStep, float a_fSpeed)
+	{
+		if (IsKeyDown(moveDownKey))
+		{
+			y -= a_fTimeStep * a_fSpeed;
+			if (y < (downExtreme))
+			{
+				y = downExtreme;
+			}
+		}
+		if (IsKeyDown(moveUpKey))
+		{
+			y += a_fTimeStep * a_fSpeed;
+			if (y >(upExtreme))
+			{
+				y = (upExtreme);
+			}
+		}
+		MoveSprite(spriteId, x, y);
+	}
+};
+struct Ball
+{
+	float dx = ballSpeedX;
+	float dy = ballSpeedY;
+	unsigned int spriteId;
+	float width;
+	float height;
+	void setSize(float a_width, float a_height)
+	{
+		width = a_width;
+		height = a_height;
+	}
+	float x;
+	float y;
+	void setPosition(float a_x, float a_y)
+	{
+		x = a_x;
+		y = a_y;
+	}
+	void moveX(float a_speed)
+	{
+		x += a_speed;
+		MoveSprite(spriteId, x, y);
+		DrawSprite(spriteId);
+	}
+	void moveY(float a_speed)
+	{
+		y += a_speed;
+		MoveSprite(spriteId, x, y);
+		DrawSprite(spriteId);
+	}
+};
+
+//Declaration of structs
+Ball ball;
+Player playerOne;
+Player playerTwo;
+Button play;
+Button instrct;
+
+
+
+GAMESTATES eCurrentState = MENU;
+int main(int argc, char* argv[])
+{
+	Initialise(screenWidth, screenHeight, false, "PONG");
+
+	//Retrieving data from I/O's to display highscores
+	fstream file;
+	file.open("PlayerOneScore.txt", ios_base::in);
+	int playerOneHighscore;
+	file >> playerOneHighscore;
+	file.sync();
+	file.close();
+	file.clear();
+
+	file.open("PlayerTwoScore.txt", ios_base::in);
+	int playerTwoHighscore;
+	file >> playerTwoHighscore;
+	file.sync();
+	file.close();
+	file.clear();
+
+	//Properties for player one
+	playerOne.width = 48.f;
+	playerOne.height = 156.f;
+	playerOne.spriteId = CreateSprite("./images/Pong_player_1.png", playerOne.width, playerOne.height, true);
+	playerOne.setMovementKeys('W', 'S');
+	playerOne.x = screenWidth * 0.15f;
+	playerOne.y = screenHeight * 0.5f;
+	playerOne.setPosition(playerOne.x, playerOne.y);
+	playerOne.upExtreme = screenHeight - (playerOne.height*.5);
+	playerOne.downExtreme = (playerOne.height*.5);
+
+	//properties for player two
+	playerTwo.width = 48.f;
+	playerTwo.height = 156.f;
+	playerTwo.spriteId = CreateSprite("./images/Pong_player_2.png", playerTwo.width, playerTwo.height, true);
+	playerTwo.setMovementKeys('O', 'L');
+	playerTwo.x = screenWidth * 0.85f;
+	playerTwo.y = screenHeight * 0.5f;
+	playerTwo.setPosition(playerTwo.x, playerTwo.y);
+	playerTwo.upExtreme = screenHeight - (playerTwo.height*.5);
+	playerTwo.downExtreme = (playerTwo.height*.5);
+
+	//properties for button with text PLAY
+	play.bX = screenWidth * 0.33;
+	play.bY = screenHeight * 0.25;
+	play.width = 250;
+	play.height = 125;
+	play.clicked = false;
+	play.spriteId = CreateSprite("./images/pong_play_button.png", play.width, play.height, true);
+	MoveSprite(play.spriteId, play.bX, play.bY);
+
+	//properties for instructions button
+	instrct.bX = screenWidth * .67;
+	instrct.bY = screenHeight * .25;
+	instrct.width = 250;
+	instrct.height = 125;
+	instrct.clicked - false;
+	instrct.spriteId = CreateSprite("./images/pong_insturctions_button.png", instrct.width, instrct.height, true);
+	MoveSprite(instrct.spriteId, instrct.bX, instrct.bY);
+
+
+	//properties for the Ball
+	ball.x = (screenWidth / 2);
+	ball.y = (screenHeight / 2);
+	ball.width = 32;
+	ball.height = 32;
+	ball.spriteId = CreateSprite("./images/Pong_ball.png", ball.width, ball.height, true);
+	MoveSprite(ball.spriteId, ball.x, ball.y);
+
+	SetBackgroundColour(SColour(0, 0, 0, 255));
+	int instrctScreen = CreateSprite("./images/instructions_screen.png", screenWidth, screenHeight, true);
+	int titeleScreen = CreateSprite("./images/title_screen.png", screenWidth, screenHeight, true);
+	MoveSprite(titeleScreen, screenWidth / 2, screenHeight / 2);
+	MoveSprite(instrctScreen, screenWidth / 2, screenHeight / 2);
+
+	POINT mouse;
+
+	//Game Loop
+	do
+	{
+		float fDeltaT = GetDeltaTime();
+		switch (eCurrentState)
+		{
+		case MENU:
+			//Tracking the mouse in order to tell if it is in contact with a button
+			GetMouseLocation(iMouseX, iMouseY);
+			DrawSprite(play.spriteId);
+			DrawSprite(instrct.spriteId);
+			DrawSprite(titeleScreen);
+			ClearScreen();
+			//Since the mouse location draws differently I couldn't figure out how to detect via the button's Y position so I just used its location from the screen height
+			if (GetMouseButtonDown(MOUSE_BUTTON_1) && iMouseX > (instrct.bX - (instrct.height / 2)) && iMouseX < (instrct.bX + (instrct.height / 2))
+				&& iMouseY >((screenHeight * .75) - (instrct.height / 2)) && iMouseY < ((screenHeight * .75) + (instrct.height / 2)))
+			{
+				instrct.clicked = true;
+			}
+			if (instrct.clicked)
+			{
+				eCurrentState = INSTRUCTIONS;
+			}
+			//Since the mouse location draws differently I couldn't figure out how to detect via the button's Y position so I just used its location from the screen height
+			if (GetMouseButtonDown(MOUSE_BUTTON_1) && iMouseX > (play.bX - (play.height / 2)) && iMouseX < (play.bX + (play.height / 2))
+				&& iMouseY >((screenHeight * .75) - (play.height / 2)) && iMouseY < ((screenHeight * .75) + (play.height / 2)))
+			{
+				play.clicked = true;
+			}
+			if (play.clicked)
+			{
+				eCurrentState = GAMEPLAY;
+			}
+			break;
+		case INSTRUCTIONS:
+			DrawSprite(instrctScreen);
+			ClearScreen();
+			break;
+		case GAMEPLAY:
+			DrawString(bufferBlue, screenWidth * .15, screenHeight * .95);
+			DrawString(bufferRed, screenWidth * .85, screenHeight * .95);
+			ball.dx = ballSpeedX;
+			ball.dy = ballSpeedY;
+			ball.moveX(ball.dx);
+			ball.moveY(ball.dy);
+			// The mess below is my half assed attempt at creating collision detection within all sides of whatever is being touched
+			if ((ball.x > screenWidth - (ball.width / 2)) || (ball.x < (ball.width / 2)) || ((playerOne.y > ball.y - playerOne.height / 2 && playerOne.y < ball.y + playerOne.height / 2) 
+				&& (playerOne.x > ball.x - ((playerOne.width / 2) + ball.width / 2) && playerOne.x < ((ball.x + playerOne.width / 2) + ball.width / 2))))
+			{
+				if (ballSpeedX < 0)
+				{
+					ballSpeedX = (ballSpeedX * -1);
+				}
+
+			}
+			if (ball.y > screenHeight - (ball.height / 2) || (ball.y < (ball.height / 2)))
+			{
+				ballSpeedY = (ballSpeedY * -1);
+			}
+			// Second verse same as the first
+			if (((playerTwo.y > ball.y - playerTwo.height / 2 && playerTwo.y < ball.y + playerTwo.height / 2) && (playerTwo.x > ball.x - ((playerTwo.width / 2) + ball.width / 2) 
+				&& playerTwo.x <((ball.x + playerTwo.width / 2) + ball.width / 2))))
+			{
+				if (ballSpeedX > 0)
+				{
+					ballSpeedX = (ballSpeedX * -1);
+				}
+			}
+			//Logic for when the ball scores against red player
+			if (ball.x > screenWidth - (ball.width / 2))
+			{
+				DestroySprite(ball.spriteId);
+				scoreBlue++;
+				ballSpeedX = (ballSpeedX * -1);
+				ball.x = screenWidth / 2;
+				ball.y = screenHeight / 2;
+				ball.spriteId = CreateSprite("./images/Pong_ball.png", ball.width, ball.height, true);
+				itoa(scoreBlue, bufferBlue, 10);
+				file.open("PlayerOneScore.txt", ios_base::out);
+				file << playerOneHighscore++;
+				file.sync();
+				file.close();
+				file.clear();
+			}
+			//Logic for when the ball scores against blue player
+			if (ball.x < (ball.width / 2))
+			{
+				DestroySprite(ball.spriteId);
+				scoreRed++;
+				ballSpeedY = (ballSpeedY * -1);
+				ball.x = screenWidth / 2;
+				ball.y = screenHeight / 2;
+				ball.spriteId = CreateSprite("./images/Pong_ball.png", ball.width, ball.height, true);
+				itoa(scoreRed, bufferRed, 10);
+				file.open("PlayerTwoScore.txt", ios_base::out);
+				playerTwoHighscore++;
+				file << playerTwoHighscore;
+				file.sync();
+				file.close();
+				file.clear();
+			}
+			//Ending for player's movements and ball movements
+			playerOne.Move(fDeltaT, speed);
+			playerTwo.Move(fDeltaT, speed);
+			DrawSprite(playerOne.spriteId);
+			DrawSprite(playerTwo.spriteId);
+			DrawSprite(ball.spriteId);
+			ClearScreen();
+			break;
+
+		}
+		
+		//gameRunning used to close the game
+	} while (!FrameworkUpdate() && gameRunning);
+
+	Shutdown();
+
+	return 0;
+}
---
+++ source/pongwork.cpp
@@ -0,0 +1,129 @@
+﻿#include "AIE.h"
+#include <iostream>
+#include <stdlibe.h>
+#include <ctime>
+
+
+int getRandomNumber(int high, int low)
+{
+	return rand() % high + low;
+}
+enum GAMESTATES
+{
+	MENU,
+	GAMEPLAY,
+	INSTRUCTIONS,
+};
+struct Player
+{
+	unsigned int spriteId;
+	float width;
+	float height;
+	void setSize(float a_width, float a_height)
+	{
+		width = a_width;
+		height = a_height;
+	}
+	float x;
+	float y;
+	void setPosition(float a_x, float a_y)
+	{
+		x = a_x;
+		y = a_y;
+	}
+	unsigned int moveUpKey;
+	unsigned int moveDownKey;
+	void setMovementKeys(unsigned int a_moveUpKey, unsigned int a_moveDownKey)
+	{
+		moveDownKey = a_moveDownKey;
+		moveUpKey = a_moveUpKey;
+	}
+	unsigned int upExtreme;
+	unsigned int downExtreme;
+	void setExtremes(float a_upExtreme, float a_downExtreme)
+	{
+		upExtreme = a_upExtreme;
+		downExtreme = a_downExtreme;
+	}
+	void Move(float a_fTimeStep, float a_fSpeed)
+	{
+		if (IsKeyDown(moveDownKey))
+		{
+			y -= a_fTimeStep * a_fSpeed;
+			if (y < (downExtreme))
+			{
+				x = downExtreme;
+			}
+		}
+		if (IsKeyDown(moveUpKey))
+		{
+			y += a_fTimeStep * a_fSpeed;
+			if (y >(upExtreme))
+			{
+				y = (upExtreme);
+			}
+		}
+		MoveSprite(spriteId, x, y);
+	}
+};
+Player playerOne;
+srand(time(NULL));
+int screenWidth = 800;
+int screenHeight = 600;
+GAMESTATES eCurrentState = MENU;
+int main( int argc, char* argv[] )
+{	
+    Initialise(screenWidth, screenHeight, false, "PONG");
+
+	//playerOne already declared globally for the case of applying the reasoning for struct Ball
+	playerOne.width = 64.f;
+	playerOne.height = 10.f;
+	playerOne.spriteId = CreateSprite("./images/Pong_player_1.png", playerOne.width, playerOne.height, true);
+	playerOne.setMovementKeys('W', 'S');
+	playerOne.x = screenWidth * 0.25f;
+	playerOne.y = screenHeight * 0.5f;
+	playerOne.setPosition(playerOne.x, playerOne.y);
+
+	struct Ball
+	{
+		unsigned int dx
+		unsigned int dy
+		unsigned int spriteId;
+		float width;
+		float height;
+		void setSize(float a_width, float a_height)
+		{
+			width = a_width;
+			height = a_height;
+		}
+		unsigned int x;
+		unsigned int y;
+		void setPosition(float a_x, float a_y)
+		{
+			x = a_x;
+			y = a_y;
+		}
+		
+		bool setDirection()
+		{
+			
+		}
+
+	};
+
+	Ball ball;
+    SetBackgroundColour(SColour(0, 0, 0, 255));
+
+    //Game Loop
+    do
+    {
+		float fDeltaT = GetDeltaTime();
+
+        ClearScreen();
+
+    } while(!FrameworkUpdate());
+
+    Shutdown();
+
+    return 0;
+}
